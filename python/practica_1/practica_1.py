30b2db2c28a30c6f0cc2025ee413d40fa78c0a27
joscor34 <zbrm34@gmail.com>
2023-02-10 22:27:50 -0600

    Se terminó el archivo de pytohn

--- python/practica_1/practica_1.py
+++ python/practica_1/practica_1.py
@@ -29,6 +29,46 @@ if(resultado == 0):
 else: 
   print('El resultado es: ' + str(resultado)+ ' y no es ortogonal')
 
+print('----- Funcion 2 -----')
+#print(mp.e)
+func1 = x**3
+func2 = x**2 + 1
+resultado = smp.integrate(func1*func2, (x, -1, 1))
+if(resultado == 0):
+  print('El resultado es: 0 y es ortogonal')
+else: 
+  print('El resultado es: ' + str(resultado)+ ' y no es ortogonal')
+
+print('----- Funcion 3 -----')
+#print(mp.e)
+func1 = (mp.e)**x
+func2 = x*((mp.e**-x) - mp.e**-x)
+resultado = smp.integrate(func1*func2, (x, 0, 2))
+if(resultado == 0):
+  print('El resultado es: 0 y es ortogonal')
+else: 
+  print('El resultado es: ' + str(resultado)+ ' y no es ortogonal')
+
+print('----- Funcion 4 -----')
+#print(mp.e)
+func1 = smp.cos(x)
+func2 = smp.sin(x)**2
+resultado = smp.integrate(func1*func2, (x, 0, mp.pi))
+if(round(resultado) == 0):
+  print('El resultado es: 0 y es ortogonal')
+else: 
+  print('El resultado es: ' + str(resultado)+ ' y no es ortogonal')
+
+print('----- Funcion 5 -----')
+#print(mp.e)
+func1 = x
+func2 = smp.cos(2*x)
+resultado = smp.integrate(func1*func2, (x, -(mp.pi/2), (mp.pi/2)))
+if(resultado == 0):
+  print('El resultado es: 0 y es ortogonal')
+else: 
+  print('El resultado es: ' + str(resultado)+ ' y no es ortogonal')
+
 
 print('----- Funcion 6 -----')
 #print(mp.e)
